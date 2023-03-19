acdb67ce049db6446c54ceb83b980e7d9e697fce
Mostafa <mostafa.omrane@proton.me>
2023-03-19 07:16:18 +0100

    Initial Commit

---
+++ Open-Me-Once/Open_Me_OnceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_Me_OnceApp.swift
+//  Open-Me-Once
+//
+//  Created by VegaPunk on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Open_Me_OnceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
